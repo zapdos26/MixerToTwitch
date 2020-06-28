45bf830fd281fce77fab01dfff34e07c8c097d44
Zapdos26 <zapdoskid@gmail.com>
2020-06-27 20:09:17 -0400

    Updated privacy policy and TOS

--- Website/src/app/app.component.ts
+++ Website/src/app/app.component.ts
@@ -1,17 +1,19 @@
-import {Component} from '@angular/core';
-import * as CryptoJS from 'crypto-js';
-
+import { Component } from "@angular/core";
+import * as CryptoJS from "crypto-js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Nexus Whitelist';
+  title = "MixerToTwitch";
   constructor() {
-    if(sessionStorage.getItem('state') == null){
-      sessionStorage.setItem('state',  CryptoJS.lib.WordArray.random(20).toString())
+    if (sessionStorage.getItem("state") == null) {
+      sessionStorage.setItem(
+        "state",
+        CryptoJS.lib.WordArray.random(20).toString()
+      );
     }
   }
 }
