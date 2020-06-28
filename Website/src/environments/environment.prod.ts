6cfe8bfcd741b23d551706deffb991ad9b18820c
Zapdos26 <zapdoskid@gmail.com>
2020-06-27 20:00:41 -0400

    Update prod environment

--- Website/src/environments/environment.prod.ts
+++ Website/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.nexuswhitelist.com",
-  domain: "https://nexuswhitelist.com",
+  apiUrl: "https://api.mixertotwitch.xyz",
+  domain: "https://mixertotwitch.xyz",
   twitchClientId: "7bb9lg2fe90h2g0cfb49mb8w9khobw",
   mixerClientId: "29b35be32515b0cce717f0d23697abf3ff48ecd230964ef5",
 };
